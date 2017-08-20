7d693c90a94b227e3ebe27cfe3ac293e4e1020b0
alexander-io <abharris@pugetsound.edu>
2017-08-20 10:51:30 -0700

    ++modify opacity rules in vector graphics

--- public/js/maze.js
+++ public/js/maze.js
@@ -27,15 +27,17 @@ let block_size_width = (two.width/dimension.s)
 let block_size_height = (two.height/dimension.s)
 
 var maze, displayMaze;
-var WALL_COLOR = '#484848';
-var WALK_COLOR = '#484848';
+var WALL_COLOR = '#484848ff';
+var WALK_COLOR = '#484848ff';
 var PATH_COLOR = 'springgreen'
 
 var setColor = function (pos, value) {
   if (value == 1) {
     displayMaze[pos.y][pos.x].fill = WALK_COLOR;
+    displayMaze[pos.y][pos.x].noStroke()
   } else if (value == 2) {
     displayMaze[pos.y][pos.x].fill = PATH_COLOR;
+    displayMaze[pos.y][pos.x].noStroke()
   }
 };
 
@@ -47,11 +49,33 @@ let renderMaze = function (maze) {
       let spot = two.makeRectangle( block_size_width*y + (block_size_width/2), block_size_height*x + (block_size_height/2), block_size_width ,block_size_height);
 
       if (maze[x][y] == 0) { // Wall
+        // XXX: added nostroke and opacity property
+        // spot.opacity = .5
         spot.fill = WALL_COLOR;
+        spot.noStroke()
+        // spot.opacity = .1
+
+        // spot.fill('#00ffffff')
+
+        // spot.noFill()
+        // spot.opacity = 0
       } else if (maze[x][y] == 1) { // Walkway
+        // XXX: added nostroke and opacity property
+        // spot.opacity = .5
         spot.fill = WALK_COLOR;
+        spot.noStroke()
+        // spot.opacity = .1
+
+        // spot.opacity = .5
+        // spot.fill('#00ffffff')
+        // spot.noFill()
+        // spot.opacity = 0
       } else if (maze[x][y] == 2) { // Solved path
+        // spot.fill('#00ffff00')
+
         spot.fill = PATH_COLOR;
+        spot.noStroke()
+        // spot.opacity = 1
       }
 
       displayRow.push(spot);
